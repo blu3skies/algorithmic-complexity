bcdc0a4ee86d61814e7013d709679045439bbccb
Joe Ellingworth <joeellingworth@gmail.com>
2020-05-15 17:09:24 +0100

    changed so arrSize is passed to find average

--- Timer.js
+++ Timer.js
@@ -1,16 +1,16 @@
 function timer(methodToTest) {
   var start = new Date().getTime();
-  methodToTest()
+  methodToTest(arrSize)
   var end = new Date().getTime();
   var time = end - start;
   return time
 }
 
-function findAverage(methodToTest) {
+function findAverage(methodToTest, arrSize) {
   var arrOfTimes = [];
   var i;
   for (i = 0; i < 50; i++) {
-    arrOfTimes.push(timer(methodToTest))
+    arrOfTimes.push(timer(methodToTest, arrSize))
   }
   var sum = arrOfTimes.reduce((a, b) => b += a)
   return (sum / arrOfTimes.length)
@@ -23,8 +23,8 @@ function createArrayAndAdd() {
   }, 0);
 }
 
-function codeToBeTested() {
-  var array = randomArray(50000) 
+function codeToBeTested(arrSize) {
+  var array = randomArray(arrSize) 
   array.pop()
 }
 
