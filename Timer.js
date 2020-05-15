be3e45f55e656e3959e80421d452bd688ffcbcda
Joe Ellingworth <joeellingworth@gmail.com>
2020-05-15 15:11:30 +0100

    refacted code

--- Timer.js
+++ Timer.js
@@ -1,24 +1,25 @@
-function timer() {
+function timer(methodToTest) {
   var start = new Date().getTime();
-
-  // code to test here
-  var array = randomArray(50000) 
-  var sum = array.reduce(function(a, b){
-      a + b;
-  }, 0);
-  // 
-
+  methodToTest
   var end = new Date().getTime();
   var time = end - start;
   return time
 }
 
-function findAverage() {
+function findAverage(methodToTest) {
   var arrOfTimes = [];
   var i;
   for (i = 0; i < 50; i++) {
-    arrOfTimes.push(timer())
+    arrOfTimes.push(timer(methodToTest))
   }
   var sum = arrOfTimes.reduce((a, b) => b += a)
   return (sum / arrOfTimes.length)
 }
+
+function createArrayAndAdd() {
+  var array = randomArray(50000) 
+  var sum = array.reduce(function(a, b){
+      a + b;
+  }, 0);
+}
+
